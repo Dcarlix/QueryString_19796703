e43e0afa776000ce06c2b3b9da9469f968f38c62
= <carlitosxd2006@gmail.com>
2025-02-20 22:28:32 -0600

    Subiendo las primera pagina y su funcionalidad del ejercicio del query string o donde las variables y la info se guardan en la URL

---
+++ QueryString_19796703/QueryString.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QueryString_19796703
+{
+    public partial class QueryString : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnBack_Click(object sender, EventArgs e)
+        {
+            Response.Redirect
+                (
+                "ProductionConfirmQueryStrings.aspx" +
+                "?ddlCategory=" + ddlCategory.SelectedValue +
+				"&ddlSupplier=" + ddlSupplier.SelectedValue +
+				"&strProduct=" + txtProduct.Text +
+				"&strDescription=" + TextArea1.InnerText +
+				"&strImage=" + txtImage.Text +
+				"&decPrice=" + txtPrice.Text +
+				"&bytNumberInStock=" + txtNumberInStock.Text +
+				"&bytNumberOnOrder=" + txtNumberOnOrder.Text +
+				"&bytReorderLevel=" + txtReorderLevel.Text
+				);
+        }
+    }
+}
